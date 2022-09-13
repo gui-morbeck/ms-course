250419f8dc7228278ed92c73b08d78d246045438
Guilherme Morbeck <morbeck719@gmail.com>
2022-09-13 15:04:25 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/com/centrooleo/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centrooleo.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
